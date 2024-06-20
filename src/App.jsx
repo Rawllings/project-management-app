73ffd51c789af3e4bbb9354023e67397607360d0
Rawllings Kiprop <rutojr01@gmail.com>
2024-06-20 16:22:35 +0300

    Done with adding projects and displaying them in sidebar. Now working with error handling with modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,51 @@
 // import NewProject from "./components/NewProject";
+import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
+import { useState } from "react";
+
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  const handleStartAddProject = () => {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  };
+
+  const handleAddProject = (projectData) => {
+    setProjectState((prevState) => {
+      const newProject = {
+        ...projectData,
+        id: Math.random(),
+      };
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  };
+
+  let content;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-2 flex gap-8">
-      <ProjectsSidebar />
-      <NoProjectSelected />
-      {/* <NewProject /> */}
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} projects={projectState.projects} />
+      {content}
     </main>
   );
 }
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useRef } from "react";
 import Input from "./Input";
 
-function NewProject() {
+function NewProject({ onAdd }) {
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  const handleSave = () => {
+    const enteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate = dueDate.current.value;
+
+    onAdd({
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate,
+    });
+  };
+
   return (
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
@@ -11,15 +27,18 @@ function NewProject() {
           </button>
         </li>
         <li>
-          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone950">
+          <button
+            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone950"
+            onClick={handleSave}
+          >
             Save
           </button>
         </li>
       </menu>
       <div>
-        <Input label="Title" />
-        <Input label="Description" textarea />
-        <Input label="Due Date" />
+        <Input type="text" ref={title} label="Title" />
+        <Input ref={description} label="Description" textarea />
+        <Input type="date" ref={dueDate} label="Due Date" />
       </div>
     </div>
   );
