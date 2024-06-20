72c753395846c7e2dd016207d981ac403ed17580
Rawllings Kiprop <rutojr01@gmail.com>
2024-06-20 09:31:49 +0300

    Added a sidebar components, and now i am going to do some styling using Tailwind CSS

---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ProjectsSidebar() {
+  return (
+    <aside>
+      <h2>Your Projects</h2>
+      <div>
+        <button>+ Add Projects</button>
+      </div>
+      <ul></ul>
+    </aside>
+  );
+}
+
+export default ProjectsSidebar;
